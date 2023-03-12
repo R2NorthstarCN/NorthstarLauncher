c2b3d5f5ac7b23bb862a7a8455745109ebcb96e9
Wtz_LASR <wtz_lasr@outlook.com>
2023-03-13 03:51:27 +0800

    i am stupid

--- include/rs.h
+++ include/rs.h
@@ -33,64 +33,68 @@ typedef struct _reed_solomon {
     unsigned char* parity;
 } reed_solomon;
 
-/**
- * MUST initial one time
- * */
-void fec_init(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-reed_solomon* reed_solomon_new(int data_shards, int parity_shards);
-void reed_solomon_release(reed_solomon* rs);
+	/**
+	 * MUST initial one time
+	 * */
+	void fec_init(void);
 
-/**
- * encode one shard
- * input:
- * rs
- * data_blocks[rs->data_shards][block_size]
- * fec_blocks[rs->data_shards][block_size]
- * */
-int reed_solomon_encode(reed_solomon* rs,
-        unsigned char** data_blocks,
-        unsigned char** fec_blocks,
-        int block_size);
+	reed_solomon* reed_solomon_new(int data_shards, int parity_shards);
+	void reed_solomon_release(reed_solomon* rs);
 
+	/**
+	 * encode one shard
+	 * input:
+	 * rs
+	 * data_blocks[rs->data_shards][block_size]
+	 * fec_blocks[rs->data_shards][block_size]
+	 * */
+	int reed_solomon_encode(reed_solomon* rs, unsigned char** data_blocks, unsigned char** fec_blocks, int block_size);
 
-/**
- * decode one shard
- * input:
- * rs
- * original data_blocks[rs->data_shards][block_size]
- * dec_fec_blocks[nr_fec_blocks][block_size]
- * fec_block_nos: fec pos number in original fec_blocks
- * erased_blocks: erased blocks in original data_blocks
- * nr_fec_blocks: the number of erased blocks
- * */
-int reed_solomon_decode(reed_solomon* rs,
-        unsigned char **data_blocks,
-        int block_size,
-        unsigned char **dec_fec_blocks,
-        unsigned int *fec_block_nos,
-        unsigned int *erased_blocks,
-        int nr_fec_blocks);
+	/**
+	 * decode one shard
+	 * input:
+	 * rs
+	 * original data_blocks[rs->data_shards][block_size]
+	 * dec_fec_blocks[nr_fec_blocks][block_size]
+	 * fec_block_nos: fec pos number in original fec_blocks
+	 * erased_blocks: erased blocks in original data_blocks
+	 * nr_fec_blocks: the number of erased blocks
+	 * */
+	int reed_solomon_decode(
+		reed_solomon* rs,
+		unsigned char** data_blocks,
+		int block_size,
+		unsigned char** dec_fec_blocks,
+		unsigned int* fec_block_nos,
+		unsigned int* erased_blocks,
+		int nr_fec_blocks);
 
-/**
- * encode a big size of buffer
- * input:
- * rs
- * nr_shards: assert(0 == nr_shards % rs->data_shards)
- * shards[nr_shards][block_size]
- * */
-int reed_solomon_encode2(reed_solomon* rs, unsigned char** shards, int nr_shards, int block_size);
+	/**
+	 * encode a big size of buffer
+	 * input:
+	 * rs
+	 * nr_shards: assert(0 == nr_shards % rs->data_shards)
+	 * shards[nr_shards][block_size]
+	 * */
+	int reed_solomon_encode2(reed_solomon* rs, unsigned char** shards, int nr_shards, int block_size);
 
-/**
- * reconstruct a big size of buffer
- * input:
- * rs
- * nr_shards: assert(0 == nr_shards % rs->data_shards)
- * shards[nr_shards][block_size]
- * marks[nr_shards] marks as errors
- * */
-int reed_solomon_reconstruct(reed_solomon* rs, unsigned char** shards, unsigned char* marks, int nr_shards, int block_size);
+	/**
+	 * reconstruct a big size of buffer
+	 * input:
+	 * rs
+	 * nr_shards: assert(0 == nr_shards % rs->data_shards)
+	 * shards[nr_shards][block_size]
+	 * marks[nr_shards] marks as errors
+	 * */
+	int reed_solomon_reconstruct(reed_solomon* rs, unsigned char** shards, unsigned char* marks, int nr_shards, int block_size);
 
-void reed_solomon_allocator(void* (*new_malloc)(size_t), void (*new_free)(void*), void* (*new_calloc)(size_t, size_t));
+	void reed_solomon_allocator(void* (*new_malloc)(size_t), void (*new_free)(void*), void* (*new_calloc)(size_t, size_t));
+#ifdef __cplusplus
+}
+#endif
 #endif
 
