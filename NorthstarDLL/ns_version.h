d4448f80b639c982bfcd83d0b6ce8d56a282fba0
wolf109909 <84360921+wolf109909@users.noreply.github.com>
2023-05-01 17:09:40 +0800

    v1.13.5

--- NorthstarDLL/ns_version.h
+++ NorthstarDLL/ns_version.h
@@ -2,7 +2,7 @@
 #ifndef NORTHSTAR_VERSION
 // Turning off clang-format here so it doesn't mess with style as it needs to be this way for regex-ing with CI
 // clang-format off
-#define NORTHSTAR_VERSION 1,13,4,0
-#define NORTHSTAR_FILE_VERSION "v1.13.4\0"
+#define NORTHSTAR_VERSION 1,13,5,0
+#define NORTHSTAR_FILE_VERSION "v1.13.5\0"
 // clang-format on
 #endif
