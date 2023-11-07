2913f01c975772a73173d4f48433495e9efb1c5a
wolf109909 <84360921+wolf109909@users.noreply.github.com>
2023-11-07 22:34:41 +0800

    svm baseline

--- NorthstarDLL/scripts/scriptsvm.h
+++ NorthstarDLL/scripts/scriptsvm.h
@@ -7,23 +7,38 @@
 SQRESULT NSSvmTrain(HSquirrelVM* sqvm)
 {
 	//g_pSquirrel<ScriptContext::SERVER>->__sq_get
+	SQArray* array = sqvm->_stackOfCurrentFunction[1]._VAL.asArray;
+	std::vector<Vector3> origins;
+
+	for (int vIdx = 0; vIdx < array->_usedSlots; ++vIdx)
+	{
+		if (array->_values[vIdx]._Type == OT_STRING)
+		{
+			origins.push_back(Vector3(
+				array->_values[vIdx]._VAL.asVector->x, array->_values[vIdx]._VAL.asVector->y, array->_values[vIdx]._VAL.asVector->z));
+		}
+	}
+
+	// origins operations
+
+
 	return SQRESULT_NOTNULL;
 }
 
 // integer NSSvmPredict(point origin)
 SQRESULT NSSvmPredict(HSquirrelVM* sqvm)
 {
-	Vector3 vec = g_pSquirrel<ScriptContext::CLIENT>->getvector(sqvm,-1);
+	Vector3 vec = g_pSquirrel<ScriptContext::CLIENT>->getvector(sqvm,1);
 
 	// code
-
-	g_pSquirrel<ScriptContext::SERVER>->pushinteger(sqvm,1);
+	int result = 0;
+	g_pSquirrel<ScriptContext::SERVER>->pushinteger(sqvm, result);
 	return SQRESULT_NOTNULL;
 }
 
 
 ON_DLL_LOAD_CLIENT_RELIESON("client.dll", ScriptPlayerInfo, ClientSquirrel, (CModule module))
 {
-	g_pSquirrel<ScriptContext::SERVER>->AddFuncRegistration("userdata", "NSSvmTrain", "", "", NSSvmTrain);
+	g_pSquirrel<ScriptContext::SERVER>->AddFuncRegistration("userdata", "NSSvmTrain", "array origins", "", NSSvmTrain);
 	g_pSquirrel<ScriptContext::SERVER>->AddFuncRegistration("int", "NSSvmPredict", "vector origin", "", NSSvmPredict);
 }
